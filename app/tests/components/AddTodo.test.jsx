bc0efcfeb348e46ce10b8947b71b57763d7579d9
Spankied <angel.owner@yahoo.com>
2017-06-07 19:47:46 -0400

    added test for todosearch

--- app/tests/components/AddTodo.test.jsx
+++ app/tests/components/AddTodo.test.jsx
@@ -11,16 +11,16 @@ describe('AddTodo', () => {
     expect(AddTodo).toExist();
   });
 
-  it('should call onAddTodo prop with valid date', () =>{
-    var todoText= 'check mail';
-    var spy= expect.createSpy();
+  it('should call onAddTodo prop with valid data', () => {
+    var todoText = 'Check mail';
+    var spy = expect.createSpy();
     var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
     var $el = $(ReactDOM.findDOMNode(addTodo));
 
     addTodo.refs.todoText.value = todoText;
     TestUtils.Simulate.submit($el.find('form')[0]);
 
-    expect(spy).toHaveBeenCalledWith('check mail');
+    expect(spy).toHaveBeenCalledWith(todoText);
   });
 
   it('should not call onAddTodo prop when invalid input', () => {
--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -1,30 +1,13 @@
-var React = require ('react');
+var React = require('react');
 var ReactDOM = require('react-dom');
-var TestUtils =require('react-addons-test-utils');
+var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jquery');
 
-var TodoList = require('TodoList');
 var Todo = require('Todo');
 
-describe('Todo', () =>{
+describe('Todo', () => {
   it('should exist', () => {
-    expect('Todo').toExist();
-  });
-
-  it('should render one Todo component for each todo item', () =>{
-    var todos = [{
-      id: 1,
-      text: 'do something'
-    }, {
-      id: 2,
-      text: 'check mail'
-    }];
-
-  var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-  var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
-
-  expect(todosComponents.length).toBe(todos.length);
-
+    expect(Todo).toExist();
   });
 });
