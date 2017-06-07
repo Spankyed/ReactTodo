b924f5f0c08afcd1507bb4f2f66126ea7670364a
Spankied <angel.owner@yahoo.com>
2017-06-07 18:28:49 -0400

    Added AddTodo component and tests

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
-var ReactDOM= require('react-dom');
+var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+var TodoApp = require('TodoApp');
 
 //Load foundation
 $(document).foundation();
@@ -9,6 +11,6 @@ $(document).foundation();
 require('style!css!sass!appStyles')
 
 ReactDOM.render(
-  <p>Boilerplate 3</p>,
+  <TodoApp/>,
   document.getElementById('app')
 );
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
+
+var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        }, {
+          id: 2,
+          text: 'Clean the yard'
+        }, {
+          id: 3,
+          text: 'Leave mail on porch'
+        }, {
+          id: 4,
+          text: 'Play video games'
+        }
+      ]
+    };
+  },
+  handleAddTodo: function (text) {
+    var newTodos = this.state.todos.slice();
+    newTodos.push(text);   
+    this.setState({todos:newTodos})
+  },
+  render: function () {
+    var {todos} = this.state;
+
+    return (
+      <div>
+        <TodoList todos={todos}/>
+        <AddTodo onAddTodo={this.handleAddTodo}/>
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
